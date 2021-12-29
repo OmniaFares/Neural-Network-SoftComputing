a0001c4b744afd4e2e7d20b7fa6825f5e0507b50
OmniaFares <omniafares133@gmail.com>
2021-12-29 21:38:50 +0200

    update weights _ mse

--- src/neuralnetwork/NeuralNetwork.java
+++ src/neuralnetwork/NeuralNetwork.java
@@ -5,7 +5,7 @@
  */
 package neuralnetwork;
 
-// import com.sun.xml.internal.bind.v2.runtime.unmarshaller.XsiNilLoader;
+
 import java.io.File;  // Import the File class
 import java.io.FileNotFoundException;  // Import this class to handle errors
 import java.util.Scanner; // Import the Scanner class to read text files
@@ -28,6 +28,7 @@ public class NeuralNetwork {
     static ArrayList<ArrayList> Ys = new ArrayList<>();
 
     static int numberofinputs, numberofhidden, numberofoutputs, numberofTrainingExamples, numberofweights;
+    static double alpha = 0.5;
 
     static ArrayList<ArrayList> output_weights = new ArrayList<>();
     static ArrayList<ArrayList> hidden_weights = new ArrayList<>();
@@ -90,9 +91,7 @@ public class NeuralNetwork {
                 } else {
                     means.add(j, (value + sumTillNow));
                 }
-
             }
-
         }
         for (int i = 0; i < numberofTrainingExamples; i++) {
             for (int j = 0; j < numberofinputs; j++) {
@@ -100,19 +99,15 @@ public class NeuralNetwork {
                 Double mean = means.get(j);
                 value = (Double) Xs.get(i).get(j);
                 sdTillNow = Sd.get(j);
-
                 Sd.remove(j);
                 if (i == numberofTrainingExamples - 1) {
                     Double Val = (value - mean) * (value - mean) + sdTillNow;
                     Val = Val / numberofTrainingExamples;
                     Sd.add(j, Math.sqrt(Val));
                 } else {
-
                     Sd.add(j, (Double) ((value - mean) * (value - mean)) + sdTillNow);
                 }
-
             }
-
         }
         for (int i = 0; i < numberofTrainingExamples; i++) {
             ArrayList<Double> newx = new ArrayList<>();
@@ -125,45 +120,27 @@ public class NeuralNetwork {
         }
 
         Xs = newxs;
-        System.out.println("Xs"+Xs);
+      //  System.out.println("Xs"+Xs);
 
     }
 
     public static void weights_initialization() {
-        // numberofweights = numberofinputs * numberofhidden + numberofhidden * numberofoutputs;
-        // Double range = (double) (1.0 / numberofweights);
-        // for (int j = 0; j < numberofhidden; j++) {
-        //     ArrayList<Double> neuron = new ArrayList<>();
-        //     for (int i = 0; i < numberofinputs; i++) {
-        //         neuron.add(Math.random() * (range + range) - range);
-        //     }
-        //     hidden_weights.add(neuron);
-        // }
-        ArrayList<Double> neuron = new ArrayList<>();
-        neuron.add(0.5);
-        neuron.add(-0.8);
-        hidden_weights.add(neuron);
-        neuron = new ArrayList<>();
-        neuron.add(0.3);
-        neuron.add(0.9);
-        hidden_weights.add(neuron);
-        neuron = new ArrayList<>();
-        // for (int j = 0; j < numberofoutputs; j++) {
-        //     ArrayList<Double> neuron = new ArrayList<>();
-        //     for (int i = 0; i < numberofhidden; i++) {
-        //         neuron.add(Math.random() * (range + range) - range);
-        //     }
-        //     output_weights.add(neuron);
-        // }
-        neuron.add(1.1);
-        neuron.add(-0.9);
-        output_weights.add(neuron);
-        neuron = new ArrayList<>();
-        neuron.add(0.8);
-        neuron.add(-1.0);
-        output_weights.add(neuron);
-        System.out.println(hidden_weights + " *** " + output_weights);
-
+        numberofweights = numberofinputs * numberofhidden + numberofhidden * numberofoutputs;
+        Double range = (double) (1.0 / numberofweights);
+        for (int j = 0; j < numberofhidden; j++) {
+            ArrayList<Double> neuron = new ArrayList<>();
+            for (int i = 0; i < numberofinputs; i++) {
+                neuron.add(Math.random() * (range + range) - range);
+            }
+            hidden_weights.add(neuron);
+        }
+        for (int j = 0; j < numberofoutputs; j++) {
+            ArrayList<Double> neuron = new ArrayList<>();
+            for (int i = 0; i < numberofhidden; i++) {
+                neuron.add(Math.random() * (range + range) - range);
+            }
+            output_weights.add(neuron);
+        }
     }
 
     public static ArrayList<ArrayList> FeedForward(int numOfCurrentTrainingExample) {
@@ -179,7 +156,6 @@ public class NeuralNetwork {
             Double H_i_out = (1 / (1 + Exp));
             Hidden_Out.add(H_i_out);
         }
-
         for (int i = 0; i < numberofoutputs; i++) {
             Double Y_i_in = 0.0;
             for (int j = 0; j < numberofhidden; j++) {
@@ -193,7 +169,6 @@ public class NeuralNetwork {
         AllOfTheAbove.add(Outputs_Out);
 
         return AllOfTheAbove;
-
     }
 
     public static void back_propagation(int numOfCurrentTrainingExample, ArrayList<Double> Hiddens_Out, ArrayList<Double> Outputs_Out){
@@ -204,7 +179,7 @@ public class NeuralNetwork {
             double error = out * (1 - out) * (target - out);
             Output_Errors.add(error);
         }
-        System.out.println("Outputs_Errors " + Output_Errors);
+        // System.out.println("Outputs_Errors " + Output_Errors);
         ArrayList<Double> Hidden_Errors = new ArrayList<>();
         for(int i=0; i<numberofhidden; i++){
             double out  = Hiddens_Out.get(i);
@@ -215,33 +190,76 @@ public class NeuralNetwork {
             double error = out * (1 - out) * sum;
             Hidden_Errors.add(error);
         }
-        System.out.println("Hidden_Errors " + Hidden_Errors);
-        update_weights(Hidden_Errors, Output_Errors);
+        // System.out.println("Hidden_Errors " + Hidden_Errors);
+        update_weights(Hidden_Errors, Output_Errors,Hiddens_Out, Outputs_Out);
     }
 
-    public static void update_weights(ArrayList<Double> Hidden_Errors,ArrayList<Double> Output_Errors){
-
+    public static void update_weights(ArrayList<Double> Hidden_Errors,ArrayList<Double> Output_Errors,  ArrayList<Double> Hiddens_Out, ArrayList<Double> Outputs_Out){
+        ArrayList<ArrayList> new_output_weights = new ArrayList<>();
+        ArrayList<ArrayList> new_hidden_weights = new ArrayList<>();
+        for(int i=0;  i<output_weights.size(); i++){
+            ArrayList<Double> neuron = new ArrayList<>();
+                for(int j=0; j<output_weights.get(i).size(); j++){
+                    double old = (Double) output_weights.get(i).get(j);
+                    double neww = old + alpha * Output_Errors.get(i) * Outputs_Out.get(i) ;
+                    neuron.add(neww);
+                }
+                new_output_weights.add(neuron);
+        }
+        output_weights.clear();
+        output_weights = new_output_weights;
+        for(int i=0;  i<hidden_weights.size(); i++){
+            ArrayList<Double> neuron = new ArrayList<>();
+                for(int j=0; j<hidden_weights.get(i).size(); j++){
+                    double old = (Double) hidden_weights.get(i).get(j);
+                    double neww = old + alpha * Hidden_Errors.get(i) * Hiddens_Out.get(i) ;
+                    neuron.add(neww);
+                }
+                new_hidden_weights.add(neuron);
+        }
+        hidden_weights.clear();
+        hidden_weights = new_hidden_weights;
     }
     public static void main(String[] args) {
-        // ArrayList<ArrayList> Outputs_AllEx = new ArrayList<>();
-        // ArrayList<ArrayList> Hiddens_AllEx = new ArrayList<>();
-        read_from_file("input_3.txt");
-       // Normalization();
+        read_from_file("train.txt");
+        Normalization();
         weights_initialization();
-        for (int j = 0; j < numberofTrainingExamples; j++) {
-            ArrayList<Double> Outputs_OneEx = new ArrayList<>();
-            ArrayList<Double> Hiddens_OneEx = new ArrayList<>();
-            ArrayList<ArrayList> Both_Hidden_Out = new ArrayList<>();
-            Both_Hidden_Out = FeedForward(j);
-            Outputs_OneEx = Both_Hidden_Out.get(1);
-            // Outputs_AllEx.add(Both_Hidden_Out.get(1));
-            Hiddens_OneEx = Both_Hidden_Out.get(0);
-            // Hiddens_AllEx.add(Both_Hidden_Out.get(0));
-            System.out.println("Training Example number " + (j + 1));
-            System.out.println("Hidden outs " + Hiddens_OneEx);
-            System.out.println("Y outs " + Outputs_OneEx);
-            back_propagation(j, Hiddens_OneEx,Outputs_OneEx);
-            System.out.println("###############################");
+        for (int iter=0; iter < 500; iter++){
+            ArrayList<ArrayList> Outputs_AllEx = new ArrayList<>();
+            for (int j = 0; j < numberofTrainingExamples; j++) {
+                ArrayList<Double> Outputs_OneEx = new ArrayList<>();
+                ArrayList<Double> Hiddens_OneEx = new ArrayList<>();
+                ArrayList<ArrayList> Both_Hidden_Out = new ArrayList<>();
+                Both_Hidden_Out = FeedForward(j);
+                Outputs_OneEx = Both_Hidden_Out.get(1);
+                Outputs_AllEx.add(Both_Hidden_Out.get(1));
+                Hiddens_OneEx = Both_Hidden_Out.get(0);
+                // System.out.println("Training Example number " + (j + 1));
+                // System.out.println("Hidden outs " + Hiddens_OneEx);
+                // System.out.println("Y outs " + Outputs_OneEx);
+                back_propagation(j, Hiddens_OneEx,Outputs_OneEx);
+                // System.out.println(hidden_weights + "***" + output_weights);
+                // System.out.println("###############################");
+            }
+            // System.out.println("Y predicted " + Outputs_AllEx);
+            // System.out.println("Y actual " + Ys);
+            ArrayList<Double> sumError = new ArrayList<>();
+            for(int i=0; i<Ys.size(); i++){
+                double sum = 0.0;
+                    for(int j=0; j<Ys.get(i).size(); j++){
+                        double diff = (Double) Ys.get(i).get(j) - (Double) Outputs_AllEx.get(i).get(j);
+                        double sq = Math.pow(diff, 2) ;
+                        sum += sq;
+                    }
+                    sumError.add(0.5*sum);
+            }
+            double sum = 0.0;
+            for(int i=0; i<sumError.size(); i++){
+                sum += sumError.get(i);
+            }
+            System.out.println("MSE in iteration " + iter + " = " + sum/numberofTrainingExamples);
         }
+        
+
     }
 }
